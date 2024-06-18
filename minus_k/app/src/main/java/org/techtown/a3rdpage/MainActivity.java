d1a3ccf2c646e4a796a7e15241630597b128e4a0
space <minu425854@gmail.com>
2024-06-18 09:00:15 +0900

    add minus_k

---
+++ minus_k/app/src/main/java/org/techtown/a3rdpage/MainActivity.java
@@ -0,0 +1,108 @@
+package org.techtown.a3rdpage;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE = 22;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        if (chkPermission()){
+            Toast.makeText(this, "위험 권한 승인함", Toast.LENGTH_SHORT).show();
+        }
+
+        //갤러리 화면으로 들어가는 임시버튼
+        Button button =findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AlbumActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean chkPermission() {
+        // 위험 권한을 모두 승인했는지 여부
+        boolean mPermissionsGranted = false;
+        // 승인 받기 위한 권한 목록
+        String[] mRequiredPermissions = new String[]{
+                Manifest.permission.READ_MEDIA_IMAGES
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 필수 권한을 가지고 있는지 확인한다.
+            mPermissionsGranted = hasPermissions(mRequiredPermissions);
+
+            // 필수 권한 중에 한 개라도 없는 경우
+            if (!mPermissionsGranted) {
+                // 권한을 요청한다.
+                ActivityCompat.requestPermissions(MainActivity.this, mRequiredPermissions, PERMISSIONS_REQUEST_CODE);
+            }
+        } else {
+            mPermissionsGranted = true;
+        }
+
+        return mPermissionsGranted;
+    }
+
+
+    public boolean hasPermissions(String[] permissions) {
+        // 필수 권한을 가지고 있는지 확인한다.
+        for (String permission : permissions) {
+            if (checkCallingOrSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            // 권한을 모두 승인했는지 여부
+            boolean chkFlag = false;
+            // 승인한 권한은 0 값, 승인 안한 권한은 -1을 값으로 가진다.
+            for (int g : grantResults) {
+                if (g == -1) {
+                    chkFlag = true;
+                    break;
+                }
+            }
+
+            // 권한 중 한 개라도 승인 안 한 경우
+            if (chkFlag){
+                chkPermission();
+            }
+        }
+    }
+
+
+}
